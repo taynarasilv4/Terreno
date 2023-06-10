4a00f48202fa7dcb84a9d9b72db498d0053838a5
Taynara <taynarasilvarodriguesr@gmail.com>
2023-06-10 12:46:38 -0400

    Terreno

---
+++ Terreno/Program.cs
@@ -0,0 +1,5 @@
+﻿Terreno terreno = new Terreno(20.0, 40.0);
+double area = terreno.Area();
+double valor = terreno.Valor(100.0);
+Console.WriteLine($"Área do terreno: {area}");
+Console.WriteLine($"o valor por metros quadrado do terreno é: {valor}");
---
+++ Terreno/Terreno.cs
@@ -0,0 +1,41 @@
+﻿public class Terreno
+{
+    private double largura;
+    private double comprimento;
+
+    public Terreno(double largura, double comprimento)
+    {
+        this.largura = largura;
+        this.comprimento = comprimento;
+    }
+
+    public double Area()
+    {
+        return largura * comprimento;
+    }
+
+    public double Valor(double valorMetroQuadrado)
+    {
+        return valorMetroQuadrado * Area();
+    }
+
+    public double GetLargura()
+    {
+       return this.largura; 
+    }
+
+    public void SetLargura(double Largura)
+    {
+        this.largura = Largura;
+    }
+    public double GetComprimento()
+    {
+        return this.comprimento;
+    }
+
+    public void SetComprimento(double Comprimento)
+    {
+        this.comprimento = Comprimento;
+    }
+   
+}
